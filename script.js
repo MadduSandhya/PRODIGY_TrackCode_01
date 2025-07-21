32c512c12aa1c7c6653343dc3d28811da744ed8d
MadduSandhya <maddusandhya99@gmail.com>
2025-07-21 11:49:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Scroll effect for navbar
+window.addEventListener("scroll", () => {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("scrolled", window.scrollY > 50);
+});
